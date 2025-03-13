7946f57c0ff4a18d661f4dd9162c0dff4ad543bb
Hughmie <hy1270371501@gmail.com>
2025-03-13 23:01:50 +0000

    data cleaning
    1. Delete the inappropriate commas
    2. Convert missing values from ? to NA
    3. Convert the type of Age and Hour_PW from char to num
    4. Delete all the rows containing missing values

---
+++ data_cleaning.R
@@ -0,0 +1,28 @@
+# load data
+data = read.csv("dataset27.csv")
+
+# inspect data
+str(data)
+summary(data)
+sapply(data, unique)
+
+# delete commas
+# convert ? to NA
+data <- data %>%
+  mutate_all(~sub(",$", "", .)) %>%
+  mutate_all(~na_if(., "?"))
+
+# inspect again
+sapply(data, unique)
+str(data)
+
+# find that the type of Age and Hour_PW are char,
+# then convert to num
+data <- data %>%
+  mutate_at(vars(Age, Hours_PW), as.numeric)
+str(data)
+
+# query and delete missing values
+colSums(is.na(data))
+data <- na.omit(data)
+str(data)
